cce7c1daaa5ab72991093aaaf320298aefe2aad2
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2024-03-13 11:04:54 -0400

    Update 0313RandomSeed.java

--- 0313RandomSeed.java
+++ 0313RandomSeed.java
@@ -21,6 +21,27 @@ public class RandomSeed {
         System.out.println("Enter a seed value:");
         int seed = sc.nextInt();
 
+        Random rand = new Random(seed);  // use the seed that the user entered
+
+        int rand1 = rand.nextInt(100);   // 0 - 99
+        System.out.println("Random number 1 = " + rand1);
+        // use the same seed, get the same "random" number!
+
+        // 10 - 50
+        int rand2 = rand.nextInt(41) + 10;
+                            //  0 - 40 + 10 = 10-50 
+        System.out.println("Random number 2 = " + rand2);
+
+        // Random chars... ASCII Values/Table
+        // ex. Random uppercase letter... ASCII value 65 - 90
+        // need to cast to a char
+
+        int rand3 = rand.nextInt(26) + 65;
+        System.out.println("Random number 3 = " + rand3);
+        char randomChar = (char)rand3;  // cast to a char
+        System.out.println("Random character = " + randomChar);
+
+
 
     }
 }
